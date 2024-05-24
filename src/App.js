d691226236924cb5a65f89c58b15fc9d29dc5cbc
ashu <ashu@ashus-MacBook-Pro.local>
2024-05-24 14:01:10 -0700

    board formed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Board from "./components/board.js"
+
+function App() {
+  return (
+    <div className="App">
+
+    Hello Chess
+
+    <br/>
+
+    <Board/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/board.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Board.css';
+const Board = () => {
+
+  function renderCell(r, c) {
+    console.log(r, c);
+    return (
+      <div key={`${r}-${c}`} className={"cell " + ((r + c) % 2 ? "white" : "grey")}></div>
+    );
+  }
+
+  function renderRow(r) {
+    let row = [];
+    for (let i = 0; i < 8; i++) {
+      row.push(renderCell(r, i));
+    }
+    return row;
+  }
+
+  function renderGrid() {
+    let grid = [];
+    for (let i = 0; i < 8; i++) {
+      grid.push(<div key={i} className="row">{renderRow(i)}</div>);
+    }
+    return grid;
+  }
+
+  return (
+    <div className='grid'>
+      {renderGrid()}
+    </div>
+  );
+};
+
+export default Board;
